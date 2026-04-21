c5209d02554180659764cfe64d91154c5c47f9d6
imamsudarajat04 <imamsudarajat708@gmail.com>
2026-04-21 11:51:05 +0700

    feat: add sitemap generator

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      sitemap({
+        hostname: 'https://imamsudarajat.my.id',
+      })
+  ],
 })
